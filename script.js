ad78d71417403dcc69bf35a61432a42724b4c6f7
Karun Binny <95923091+lykanranger@users.noreply.github.com>
2026-04-19 12:46:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function () {
   setTimeout(() => {
     loadingContainer.style.display = 'none';
     islandBar.classList.add('show-airpods');
-  }, 4700);
+  }, 6700);
 });
 
 // TYPING ANIMATION
@@ -25,7 +25,7 @@ var year = new Date().getFullYear();
 
 var typed = new Typed('.typing-text', {
   strings: [
-    'Created by <span class="highlight-name">KΛRUN</span>',
+    'Created by Karun',
     `© ${year} All Rights Reserved`
   ],
   typeSpeed: 100,
@@ -47,3 +47,63 @@ window.addEventListener('scroll', function() {
   }
 });
 
+// PARTICLES.JS INITIALIZATION
+
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 100,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": ["#000000", "#777777"]
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      }
+    },
+    "opacity": {
+      "value": 0.3,
+      "random": false
+    },
+    "size": {
+      "value": 3,
+      "random": true
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 150,
+      "color": "#000000",
+      "opacity": 0.3,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 2,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false
+      },
+      "onclick": {
+        "enable": false
+      },
+      "resize": true
+    }
+  },
+  "retina_detect": true
+});
